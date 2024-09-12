95f85adf40537c48e0fa57f502af7678f80c3d40
AnthonySTZ <anthonycandy2309@gmail.com>
2024-09-12 23:54:24 +0200

    implement LR

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -2,9 +2,38 @@ import numpy as np
 
 
 def gradient_descent_runner(
-    points, initial_b, initial_m, learning_rate, num_iterations
+    points, starting_b, starting_m, learning_rate, num_iterations
 ):
-    pass
+    # Starting m and b
+    b = starting_b
+    m = starting_m
+
+    # Gradient descent
+    for _ in range(num_iterations):
+        # gradient step
+        b, m = gradient_step(b, m, points, learning_rate)
+
+    return b, m
+
+
+def gradient_step(b_current, m_current, points, learning_rate):
+
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))  # Number of points
+
+    for pt in points:
+        x, y = pt
+
+        # Direction respect to b and m
+        # Computing partial derivatives of our error function
+
+        b_gradient += -(2 / N) * (y - ((m_current * x) + b_current))
+        m_gradient += -(2 / N) * x * (y - ((m_current * x) + b_current))
+
+    b_new = b_current - (b_gradient * learning_rate)
+    m_new = m_current - (m_gradient * learning_rate)
+    return b_new, m_new
 
 
 def compute_error_for_line_given_points(b, m, points):
@@ -34,10 +63,10 @@ def compute_linear_regression(filename):
     print(
         f"Starting gradient descent at b={initial_b}, m={initial_m}, error={compute_error_for_line_given_points(initial_b, initial_m, points)}"
     )
-    # computed_b, computed_m = gradient_descent_runner(
-    #     points, initial_b, initial_m, learning_rate, num_iterations
-    # )
+    computed_b, computed_m = gradient_descent_runner(
+        points, initial_b, initial_m, learning_rate, num_iterations
+    )
 
-    # print(
-    #     f"Ending point after {num_iterations} iterations, at b={computed_b}, m={computed_m}, error={compute_error_for_line_given_points(computed_b, computed_m, points)}"
-    # )
+    print(
+        f"Ending point after {num_iterations} iterations, at b={computed_b}, m={computed_m}, error={compute_error_for_line_given_points(computed_b, computed_m, points)}"
+    )
